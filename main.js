7c0966a8887ba0a45f10221c6b0d56dfe029a40f
Kryspin Jemiolo <100538163+KryspinJemiolo@users.noreply.github.com>
2022-03-25 19:49:13 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// In this file, we will be calling our crhome API's
+/* In this file, we have an event listener, which listens before we want to send a request. This event listener, will then check
+where the request is going. If it is an unwanted URL, it will block the request */
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true}},
-    /* To block more sites/ads, copy and paste the below example*/
+    /* To block more sites/ads, copy and paste the URL(s) like shown in the example below*/
     { urls: 
         ["*://*.adroll.com/*", "*://*.g2.com/*", "*://*.totaladblock.com/*"]},
         ["blocking"]
