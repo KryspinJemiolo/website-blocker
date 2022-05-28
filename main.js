79a8f861b4ceaa1b8e3ee8049312f60faa8720e9
Kryspin <100538163+KryspinJemiolo@users.noreply.github.com>
2022-05-28 10:24:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true}},
-    /* To block more sites/ads, copy and paste the URL(s) like shown in the example below*/
+    /* To block more sites/ads, copy and paste the URL(s) like shown in the example below. */
     { urls: 
         ["*://*.adroll.com/*", "*://*.g2.com/*", "*://*.totaladblock.com/*"]},
         ["blocking"]
